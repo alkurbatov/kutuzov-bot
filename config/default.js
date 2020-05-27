f199ffbe340f236f92aca588a140f486b2154f20
Alexander Kurbatov <Alexander.Kurbatov@acronis.com>
2020-05-27 10:31:56 +0300

    docs: Document possible configuration options
    Signed-off-by: Alexander Kurbatov <Alexander.Kurbatov@acronis.com>

--- config/default.js
+++ config/default.js
@@ -1,9 +1,21 @@
 module.exports = {
+  // Race kutuzov-bot plays for.
+  // Possible values:
+  // TERRAN, ZERG, PROTOSS, RANDOM.
+  // (!) Currently, only TERRAN is supported.
   race: 'TERRAN',
 
   // Settings of computer oppenent.
   opponent: {
+    // Difficulty of a compoute opponent.
+    // Possible values:
+    // VERYEASY, EASY, MEDIUM, MEDIUMHARD,
+    // HARD, HARDER, VERYHARD, CHEATVISION, CHEATMONEY, CHEATINSANE
     difficulty: undefined,
+
+    // Race of the computer opponent.
+    // Possible values:
+    // TERRAN, ZERG, PROTOSS, RANDOM.
     race: undefined,
   },
 
